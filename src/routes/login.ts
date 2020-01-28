c7d7da0409a6d01372dfaf74d17f479612ac6849
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-28 11:51:43 +0100

    modify login route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,13 +1,30 @@
 import { Router } from 'express';
+import { loginUser } from '../controllers/login';
 
 const router = Router();
 
-router.get('/login', function(_req, res, _next) {
-  res.status(200).json({ message: 'You hit the login route' });
-});
+router.post('/login', async (req, res, _next) => {
+  const { username, password } = req.body;
 
-router.post('/login', function(_req, res, _next) {
-  res.status(200).json({ message: 'You intended to login' });
+  if (username.trim() === '' || password.trim() === '') {
+    res.status(400).json({ message: 'You need to provide all details' });
+
+    return;
+  }
+
+  try {
+    const userDetails = await loginUser(username, password);
+
+    const { status, message, payload, token } = userDetails;
+
+    res.status(status).json({
+      message,
+      payload,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
