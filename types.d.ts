fad349947a1b6b0ddef146f95ff340914746bd8a
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-28 11:41:39 +0100

    add interface for user

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+export type iUser = {
+  lastName: string;
+  firstName: string;
+  email: string;
+  mobileNumber: string;
+  password: string;
+  confirmPassword: string;
+  _accounts?: object[];
+};
