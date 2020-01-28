8ddf756b24d05dfce239640e49c7e3fc4bbdaaac
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-28 11:41:01 +0100

    modify route

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
+import { registerUser } from '../controllers/register';
+// import { iUser } from '../../types';
 
 const router = Router();
 
-router.get('/register', function(_req, res, _next) {
+router.get('/register', async (_req, res, _next) => {
   res.status(200).json({ message: 'You hit the register route' });
 });
 
-router.post('/register', function(_req, res, _next) {
-  res.status(200).json({ message: "You've tried to register" });
+router.post('/register', async (req, res, _next) => {
+  try {
+    const newUser = await registerUser(req.body);
+
+    if (!newUser) {
+      res.status(500).json({ message: 'Registration was not successful' });
+
+      return;
+    }
+
+    res.status(200).json({ newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
