fdf4551fbbc9ccaca4dc014ce92947a5cfcb9342
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-28 17:12:53 +0100

    create basic home page

---
+++ client/src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+function Home() {
+  return (
+    <Container textAlign="center">
+      <h1>Welcome to miniBank!</h1>
+    </Container>
+  );
+}
+
+export default Home;
