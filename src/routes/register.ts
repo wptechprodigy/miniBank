04bdf7b7e3c397dec4d3417144dee04e9fbd73ab
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-28 07:26:43 +0100

    add basic route to register

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -6,4 +6,8 @@ router.get('/register', function(_req, res, _next) {
   res.status(200).json({ message: 'You hit the register route' });
 });
 
+router.post('/register', function(_req, res, _next) {
+  res.status(200).json({ message: "You've tried to register" });
+});
+
 export default router;
